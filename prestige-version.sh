6b92edd93f30e7b0dda55ee69eebd36566f451f9
Jérémy M <jmisiti@student.42.fr>
2019-07-12 18:27:44 +0200

    change(devops): Force pusher to build before

--- prestige-version.sh
+++ prestige-version.sh
@@ -7,16 +7,29 @@ then
     TRELLOID=$3
     DESC=$4
     BRANCHE=$(git rev-parse --abbrev-ref HEAD)
+    ANDROID="latest.apk"
+    IOS="latest.apk"
+    OLDTIME=500
+    CURTIME=$(date +%s)
+    ANDROIDTIME=$(stat -t %s -f %m $ANDROID)
+    ANDROIDDIFF=$(expr $CURTIME - $ANDROIDTIME)
+    IOSTIME=$(stat -t %s -f %m $IOS)
+    IOSDIFF=$(expr $CURTIME - $IOSTIME)
 
-    if [ ! "$BRANCHE" = "$TYPE/$SCOPE/$TRELLOID" ];
+    if [ "$ANDROIDDIFF" -lt "$OLDTIME" ] || [ "$IOSDIFF" -lt "$OLDTIME" ];
     then
-        git checkout -b "$TYPE/$SCOPE/$TRELLOID"
-    fi
+        if [ ! "$BRANCHE" = "$TYPE/$SCOPE/$TRELLOID" ];
+        then
+            git checkout -b "$TYPE/$SCOPE/$TRELLOID"
+        fi
 
-    git add .
-    git commit -m "$TYPE($SCOPE): $DESC"
-    git push --set-upstream origin "$TYPE/$SCOPE/$TRELLOID"
-    git checkout master
+        git add .
+        git commit -m "$TYPE($SCOPE): $DESC"
+        git push --set-upstream origin "$TYPE/$SCOPE/$TRELLOID"
+        git checkout master
+    else
+        echo "You have to build the project for IOS or Android before !"
+    fi
 else
     echo "Wrong usage, please use :\n./prestige-version.sh [fix, feature, revert, change] [character, monsters, server, etc..] [trelloId] [description]"
 fi
