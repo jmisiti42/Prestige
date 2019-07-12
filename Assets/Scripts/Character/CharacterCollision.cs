080fc0aba5744ff81763b6d8b177270bc71b3429
Thibault Lux <tlux@student.42.fr>
2019-07-12 16:51:42 +0200

    Add Collision Script and HP class, Collider2D elements on mage with RigidBody 2D. Add sample fireball

---
+++ Assets/Scripts/Character/CharacterCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollision : MonoBehaviour
+{
+    private HP hp;
+
+    void Start()
+    {
+        hp = new HP(2f);
+        Debug.Log(hp.hp);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        hp.hp -= 0.5f; // Todo find a better name
+        Debug.Log(hp.hp);
+        Destroy(collision.gameObject);
+    }
+    void Update()
+    {
+    }
+}
