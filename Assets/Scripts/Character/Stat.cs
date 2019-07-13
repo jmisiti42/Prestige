5c8a2c271b33990fb9c07d42e145071e866e70c4
Jérémy M <jmisiti@student.42.fr>
2019-07-13 15:04:45 +0200

    change(C#): Modification stats

---
+++ Assets/Scripts/Character/Stat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat
+{
+    public float value;
+
+    public Stat(float start)
+    {
+        value = start;
+    }
+
+    public void Add(float plus)
+    {
+        value += plus;
+    }
+
+    public void Remove(float minus)
+    {
+        value -= minus;
+    }
+}
---
+++ Assets/Scripts/Character/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    private Stat hp;
+    private Stat damage;
+
+    void Start()
+    {
+        hp = new Stat(2f);
+        damage = new Stat(3f);
+    }
+}
