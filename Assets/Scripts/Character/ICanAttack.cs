20e6dc414444cbebee0b2bc14706e04a132e6e06
Jérémy M <jmisiti@student.42.fr>
2019-07-15 20:14:40 +0200

    feature(pYAH2l8K):

--- Assets/Scripts/Character/ICanAttack.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface ICanAttack
-{
-    void Attack();
-}
---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    public Stat hp, damage, speed, attackDelay;
+
+    public Enemy(float startHp, float startDamage, float startSpeed, float startAttackDelay)
+    {
+        hp = new Stat(startHp);
+        damage = new Stat(startDamage);
+        speed = new Stat(startSpeed);
+        attackDelay = new Stat(startAttackDelay);
+    }
+}
